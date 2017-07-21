2288affbddd99d49e859e73623f7e1f745732434
Asım Arslan <arslanasim@gmail.com>
2017-07-21 12:44:23 +0300

    build script

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Outside tools include:
+# gocov: go get github.com/axw/gocov
+# gocov-xml: go get github.com/t-yuki/gocov-xml
+# go2xunit: go get bitbucket.org/tebeka/go2xunit
+
+set -x
+
+# Set up environment
+export PRJ=`git config --get remote.origin.url | sed 's/^https:\/\///' | sed 's/\.git$//'`
+
+go get git.apache.org/thrift.git/lib/go/thrift
+
+# Run tests (JUnit plugin)
+echo "mode: set" > coverage.out
+for pkg in $(go list $PRJ/...);
+do
+    if [[ $pkg != *"vendor"* ]]; then
+      echo "testing... $pkg"
+      go test -v -coverprofile=tmp.out $pkg >> test.out
+      if [ -f tmp.out ]; then
+         cat tmp.out | grep -v "mode: set" >> coverage.out
+      fi
+    fi
+done
+rm -f ./tmp.out
+cat test.out | go2xunit -output tests.xml
+
+# Generate coverage reports (Cobertura plugin)
+gocov convert coverage.out | gocov-xml > cobertura-coverage.xml
+
+# Run vet tools (Compiler warning plugin)
+go vet $PRJ > vet.txt
+
+## Run lint tools (Compiler warning plugin)
+#golint $PRJ > lint.txt
