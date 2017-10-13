4d85a6917ff912ae1d2fb317304e341371c1c1b5
talha <s.talhanisanci@gmail.com>
2017-10-13 21:31:09 +0300

    uni socket with multiple members is tested with more operations.

--- tests/invocation_test.go
+++ tests/invocation_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"github.com/hazelcast/go-client"
 	"github.com/hazelcast/go-client/internal"
+	"strconv"
 	"testing"
 )
 
@@ -20,6 +21,7 @@ func TestNonSmartInvoke(t *testing.T) {
 	res, err := mp.Get(testKey)
 	AssertEqualf(t, err, res, testValue, "get returned a wrong value")
 	mp.Clear()
+	remoteController.ShutdownCluster(cluster.ID)
 }
 func TestSingleConnectionWithManyMembers(t *testing.T) {
 	cluster, _ = remoteController.CreateCluster("3.9", DEFAULT_XML_CONFIG)
@@ -29,6 +31,16 @@ func TestSingleConnectionWithManyMembers(t *testing.T) {
 	config := hazelcast.NewHazelcastConfig()
 	config.ClientNetworkConfig.SmartRouting = false
 	client := hazelcast.NewHazelcastClientWithConfig(config)
+	mapName := "testMap"
+	mp := client.GetMap(&mapName)
+	for i := 0; i < 100; i++ {
+		testKey := "testingKey" + strconv.Itoa(i)
+		testValue := "testingValue" + strconv.Itoa(i)
+		mp.Put(testKey, testValue)
+		res, err := mp.Get(testKey)
+		AssertEqualf(t, err, res, testValue, "get returned a wrong value")
+	}
+	mp.Clear()
 	connectionCount := client.(*internal.HazelcastClient).ConnectionManager.ConnectionCount()
 	AssertEqualf(t, nil, int32(1), connectionCount, "Client should open only one connection")
 	remoteController.ShutdownCluster(cluster.ID)
