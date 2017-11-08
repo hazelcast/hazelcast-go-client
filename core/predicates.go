dbead1d9a0b2c36998f255f2654598154610dad3
Furkan Şenharputlu <furkan_senharputlu@hotmail.com>
2017-11-08 15:53:58 +0200

    Built-in predicates are implemented and tested

--- core/predicates.go
+++ core/predicates.go
@@ -9,3 +9,67 @@ func Sql(sql string) IPredicate {
 func And(predicates []IPredicate) IPredicate {
 	return NewAndPredicate(predicates)
 }
+
+func Between(field string, from interface{}, to interface{}) IPredicate {
+	return NewBetweenPredicate(field, from, to)
+}
+
+func Equal(field string, value interface{}) IPredicate {
+	return NewEqualPredicate(field, value)
+}
+
+func GreaterThan(field string, value interface{}) IPredicate {
+	return NewGreaterLessPredicate(field, value, false, false)
+}
+
+func GreaterEqual(field string, value interface{}) IPredicate {
+	return NewGreaterLessPredicate(field, value, true, false)
+}
+
+func LessThan(field string, value interface{}) IPredicate {
+	return NewGreaterLessPredicate(field, value, false, true)
+}
+
+func LessEqual(field string, value interface{}) IPredicate {
+	return NewGreaterLessPredicate(field, value, true, true)
+}
+
+func Like(field string, expr string) IPredicate {
+	return NewLikePredicate(field, expr)
+}
+
+func ILike(field string, expr string) IPredicate {
+	return NewILikePredicate(field, expr)
+}
+
+func In(field string, values []interface{}) IPredicate {
+	return NewInPredicate(field, values)
+}
+
+func InstanceOf(className string) IPredicate {
+	return NewInstanceOfPredicate(className)
+}
+
+func NotEqual(field string, value interface{}) IPredicate {
+	return NewNotEqualPredicate(field, value)
+}
+
+func Not(predicate IPredicate) IPredicate {
+	return NewNotPredicate(predicate)
+}
+
+func Or(predicates []IPredicate) IPredicate {
+	return NewOrPredicate(predicates)
+}
+
+func Regex(field string, regex string) IPredicate {
+	return NewRegexPredicate(field, regex)
+}
+
+func True() IPredicate {
+	return NewTruePredicate()
+}
+
+func False() IPredicate {
+	return NewFalsePredicate()
+}
