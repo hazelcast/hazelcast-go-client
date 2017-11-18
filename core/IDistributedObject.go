553cc7831ceb9f969a7fda4392302094e906efb8
talha <s.talhanisanci@gmail.com>
2017-11-18 14:45:51 +0300

    add apiDoc for IMap IDistributedObject ILifecycle and ICluster

--- core/IDistributedObject.go
+++ core/IDistributedObject.go
@@ -1,8 +1,19 @@
 package core
 
+// IDistributedObject is the base interface for all distributed objects.
 type IDistributedObject interface {
+	// Destroy destroys this object cluster-wide.
+	// Destroy clears and releases all resources for this object.
 	Destroy() (bool, error)
+
+	// Name returns the unique name for this IDistributedObject. Returned value will never be nil.
 	Name() string
+
+	// PartitionKey returns the key of partition this IDistributedObject is assigned to. The returned value only has meaning
+	// for a non partitioned data structure like an IAtomicLong. For a partitioned data structure like an IMap
+	// the returned value will not be nil, but otherwise undefined.
 	PartitionKey() string
+
+	// ServiceName returns the service name for this object.
 	ServiceName() string
 }
