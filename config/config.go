b9bcb25db6368329a0f71a9f96b51caf8ce0809e
Furkan Şenharputlu <furkan_senharputlu@hotmail.com>
2017-11-19 22:29:31 +0200

    ApiDoc for remaining part of config

--- config/config.go
+++ config/config.go
@@ -1,3 +1,4 @@
+// Config package contains all the configuration to start a Hazelcast Instance
 package config
 
 import (
@@ -13,16 +14,25 @@ const (
 	DEFAULT_INVOCATION_TIMEOUT = 120 * time.Second //secs
 )
 
-////////////////////// ClientConfig //////////////////////////////
-
+// ClientConfig is the main configuration to setup a Hazelcast Client.
 type ClientConfig struct {
+	// membershipListeners is the array of cluster membership listeners.
 	membershipListeners []interface{}
-	lifecycleListeners  []interface{}
-	groupConfig         *GroupConfig
+
+	// lifecycleListeners is the array of listeners for listening to lifecycle events of the Hazelcast Instance.
+	lifecycleListeners []interface{}
+
+	// groupConfig is the configuration for Hazelcast groups.
+	groupConfig *GroupConfig
+
+	// clientNetworkConfig is the network configuration of the client.
 	clientNetworkConfig *ClientNetworkConfig
+
+	// serializationConfig is the serialization configuration of the client.
 	serializationConfig *SerializationConfig
 }
 
+// NewClientConfig creates a new ClientConfig with default configuration.
 func NewClientConfig() *ClientConfig {
 	return &ClientConfig{groupConfig: NewGroupConfig(),
 		clientNetworkConfig: NewClientNetworkConfig(),
@@ -32,102 +42,135 @@ func NewClientConfig() *ClientConfig {
 	}
 }
 
+// MembershipListeners returns membership listeners.
 func (clientConfig *ClientConfig) MembershipListeners() []interface{} {
 	return clientConfig.membershipListeners
 }
 
+// LifecycleListeners returns lifecycle listeners.
 func (clientConfig *ClientConfig) LifecycleListeners() []interface{} {
 	return clientConfig.lifecycleListeners
 }
 
+// GroupConfig returns GroupConfig.
 func (clientConfig *ClientConfig) GroupConfig() *GroupConfig {
 	return clientConfig.groupConfig
 }
 
+// ClientNetworkConfig returns ClientNetworkConfig.
 func (clientConfig *ClientConfig) ClientNetworkConfig() *ClientNetworkConfig {
 	return clientConfig.clientNetworkConfig
 }
 
+// SerializationConfig returns SerializationConfig.
 func (clientConfig *ClientConfig) SerializationConfig() *SerializationConfig {
 	return clientConfig.serializationConfig
 }
 
+// AddMembershipListener adds a membership listener.
 func (clientConfig *ClientConfig) AddMembershipListener(listener interface{}) {
 	clientConfig.membershipListeners = append(clientConfig.membershipListeners, listener)
 }
 
+// AddLifecycleListener adds a lifecycle listener.
 func (clientConfig *ClientConfig) AddLifecycleListener(listener interface{}) {
 	clientConfig.lifecycleListeners = append(clientConfig.lifecycleListeners, listener)
 }
 
+// SetGroupConfig sets the GroupConfig.
 func (clientConfig *ClientConfig) SetGroupConfig(groupConfig *GroupConfig) {
 	clientConfig.groupConfig = groupConfig
 }
 
+// SetClientNetworkConfig sets the ClientNetworkConfig.
 func (clientConfig *ClientConfig) SetClientNetworkConfig(clientNetworkConfig *ClientNetworkConfig) {
 	clientConfig.clientNetworkConfig = clientNetworkConfig
 }
 
+// SetSerializationConfig sets the SerializationConfig.
 func (clientConfig *ClientConfig) SetSerializationConfig(serializationConfig *SerializationConfig) {
 	clientConfig.serializationConfig = serializationConfig
 }
 
-////////////////////// SerializationConfig //////////////////////////////
-
+// SerializationConfig contains the serialization configuration of a Hazelcast Instance.
 type SerializationConfig struct {
-	isBigEndian               bool
+	// isBigEndian is the byte order bool. If true, it means BigEndian, otherwise LittleEndian.
+	isBigEndian bool
+
+	// dataSerializableFactories is a map of factory IDs and corresponding IdentifiedDataSerializable factories.
 	dataSerializableFactories map[int32]IdentifiedDataSerializableFactory
-	portableFactories         map[int32]PortableFactory
-	portableVersion           int32
-	customSerializers         map[reflect.Type]Serializer
-	globalSerializer          Serializer
+
+	// portableFactories is a map of factory IDs and corresponding Portable factories.
+	portableFactories map[int32]PortableFactory
+
+	// Portable version will be used to differentiate two versions of the same struct that have changes on the struct,
+	// like adding/removing a field or changing a type of a field.
+	portableVersion int32
+
+	// customSerializers is a map of object types and corresponding custom serializers.
+	customSerializers map[reflect.Type]Serializer
+
+	// globalSerializer is the serializer that will be used if no other serializer is applicable.
+	globalSerializer Serializer
 }
 
+// NewSerializationConfig creates a SerializationConfig with default values.
 func NewSerializationConfig() *SerializationConfig {
 	return &SerializationConfig{isBigEndian: true, dataSerializableFactories: make(map[int32]IdentifiedDataSerializableFactory),
 		portableFactories: make(map[int32]PortableFactory), portableVersion: 0, customSerializers: make(map[reflect.Type]Serializer)}
 }
 
+// IsBigEndian returns isBigEndian bool value.
 func (serializationConfig *SerializationConfig) IsBigEndian() bool {
 	return serializationConfig.isBigEndian
 }
 
+// DataSerializableFactories returns a map of factory IDs and corresponding IdentifiedDataSerializable factories.
 func (serializationConfig *SerializationConfig) DataSerializableFactories() map[int32]IdentifiedDataSerializableFactory {
 	return serializationConfig.dataSerializableFactories
 }
 
+// PortableFactories returns a map of factory IDs and corresponding Portable factories.
 func (serializationConfig *SerializationConfig) PortableFactories() map[int32]PortableFactory {
 	return serializationConfig.portableFactories
 }
 
+// PortableVersion returns version of a portable struct.
 func (serializationConfig *SerializationConfig) PortableVersion() int32 {
 	return serializationConfig.portableVersion
 }
 
+// CustomSerializers returns a map of object types and corresponding custom serializers.
 func (serializationConfig *SerializationConfig) CustomSerializers() map[reflect.Type]Serializer {
 	return serializationConfig.customSerializers
 }
 
+// GlobalSerializer returns the global serializer.
 func (serializationConfig *SerializationConfig) GlobalSerializer() Serializer {
 	return serializationConfig.globalSerializer
 }
 
+// SetByteOrder sets the byte order. If true, it means BigEndian, otherwise LittleEndian.
 func (serializationConfig *SerializationConfig) SetByteOrder(isBigEndian bool) {
 	serializationConfig.isBigEndian = isBigEndian
 }
 
+// AddDataSerializableFactory adds a IdentifiedDataSerializableFactory for a given factory ID.
 func (serializationConfig *SerializationConfig) AddDataSerializableFactory(factoryId int32, f IdentifiedDataSerializableFactory) {
 	serializationConfig.dataSerializableFactories[factoryId] = f
 }
 
+// AddPortableFactory adds a PortableFactory for a given factory ID.
 func (serializationConfig *SerializationConfig) AddPortableFactory(factoryId int32, pf PortableFactory) {
 	serializationConfig.portableFactories[factoryId] = pf
 }
 
+// SetPortableVersion sets the portable version.
 func (serializationConfig *SerializationConfig) SetPortableVersion(version int32) {
 	serializationConfig.portableVersion = version
 }
 
+// AddCustomSerializer adds a custom serializer for a given type. It can be an interface type or a struct type.
 func (serializationConfig *SerializationConfig) AddCustomSerializer(typ reflect.Type, serializer Serializer) error {
 	if serializer.Id() > 0 {
 		serializationConfig.customSerializers[typ] = serializer
@@ -137,6 +180,7 @@ func (serializationConfig *SerializationConfig) AddCustomSerializer(typ reflect.
 	return nil
 }
 
+// SetGlobalSerializer sets the global serializer.
 func (serializationConfig *SerializationConfig) SetGlobalSerializer(serializer Serializer) error {
 	if serializer.Id() > 0 {
 		serializationConfig.globalSerializer = serializer
