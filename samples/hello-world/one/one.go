7e16a03cdc9948e12f2e43c4416a117b594c7f1b
Neil Stevenson <neilstevenson@users.noreply.github.com>
2017-12-20 07:32:02 +0000

    Initial example blog post (#127)
    Initial example blog post

---
+++ samples/hello-world/one/one.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+import "github.com/hazelcast/hazelcast-go-client"
+
+/* This is the bare minimum test to see if we can connect.
+ *
+ * Steps here are:
+ *
+ * (1) Create 'clientConfig', configuration for the connection
+ * (2) Add the server's host & port to the connection configuration
+ * (3) Create a client using this configuration
+ * (4) Disconnect the client and shut down.
+ */
+
+func main() {
+	clientConfig := hazelcast.NewHazelcastConfig()
+	fmt.Println("Cluster name: ", clientConfig.GroupConfig().Name())
+
+	clientConfig.ClientNetworkConfig().AddAddress("127.0.0.1:5701")
+	fmt.Println("Cluster discovery: ", clientConfig.ClientNetworkConfig().Addresses())
+
+	client, _ := hazelcast.NewHazelcastClientWithConfig(clientConfig)
+
+	client.Shutdown()
+}
