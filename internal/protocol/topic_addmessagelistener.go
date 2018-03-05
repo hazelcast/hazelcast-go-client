02c6158e1a74b173a8bd657776d5bf9623d19cda
furkansenharputlu <furkan_senharputlu@hotmail.com>
2018-03-06 00:40:24 +0300

    implement ITopic with tests

---
+++ internal/protocol/topic_addmessagelistener.go
@@ -0,0 +1,66 @@
+// Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+import (
+	. "github.com/hazelcast/hazelcast-go-client/internal/serialization"
+
+	. "github.com/hazelcast/hazelcast-go-client/internal/common"
+)
+
+func TopicAddMessageListenerCalculateSize(name *string, localOnly bool) int {
+	// Calculates the request payload size
+	dataSize := 0
+	dataSize += StringCalculateSize(name)
+	dataSize += BOOL_SIZE_IN_BYTES
+	return dataSize
+}
+
+func TopicAddMessageListenerEncodeRequest(name *string, localOnly bool) *ClientMessage {
+	// Encode request into clientMessage
+	clientMessage := NewClientMessage(nil, TopicAddMessageListenerCalculateSize(name, localOnly))
+	clientMessage.SetMessageType(TOPIC_ADDMESSAGELISTENER)
+	clientMessage.IsRetryable = false
+	clientMessage.AppendString(name)
+	clientMessage.AppendBool(localOnly)
+	clientMessage.UpdateFrameLength()
+	return clientMessage
+}
+
+func TopicAddMessageListenerDecodeResponse(clientMessage *ClientMessage) func() (response *string) {
+	// Decode response from client message
+	return func() (response *string) {
+		response = clientMessage.ReadString()
+		return
+	}
+}
+
+type TopicAddMessageListenerHandleEventTopicFunc func(*Data, int64, *string)
+
+func TopicAddMessageListenerEventTopicDecode(clientMessage *ClientMessage) (item *Data, publishTime int64, uuid *string) {
+	item = clientMessage.ReadData()
+	publishTime = clientMessage.ReadInt64()
+	uuid = clientMessage.ReadString()
+	return
+}
+
+func TopicAddMessageListenerHandle(clientMessage *ClientMessage,
+	handleEventTopic TopicAddMessageListenerHandleEventTopicFunc) {
+	// Event handler
+	messageType := clientMessage.MessageType()
+	if messageType == EVENT_TOPIC && handleEventTopic != nil {
+		handleEventTopic(TopicAddMessageListenerEventTopicDecode(clientMessage))
+	}
+}
---
+++ internal/protocol/topic_removemessagelistener.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protocol
+
+import ()
+
+func TopicRemoveMessageListenerCalculateSize(name *string, registrationId *string) int {
+	// Calculates the request payload size
+	dataSize := 0
+	dataSize += StringCalculateSize(name)
+	dataSize += StringCalculateSize(registrationId)
+	return dataSize
+}
+
+func TopicRemoveMessageListenerEncodeRequest(name *string, registrationId *string) *ClientMessage {
+	// Encode request into clientMessage
+	clientMessage := NewClientMessage(nil, TopicRemoveMessageListenerCalculateSize(name, registrationId))
+	clientMessage.SetMessageType(TOPIC_REMOVEMESSAGELISTENER)
+	clientMessage.IsRetryable = true
+	clientMessage.AppendString(name)
+	clientMessage.AppendString(registrationId)
+	clientMessage.UpdateFrameLength()
+	return clientMessage
+}
+
+func TopicRemoveMessageListenerDecodeResponse(clientMessage *ClientMessage) func() (response bool) {
+	// Decode response from client message
+	return func() (response bool) {
+		response = clientMessage.ReadBool()
+		return
+	}
+}
