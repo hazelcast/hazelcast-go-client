64a617fff8816c2686e91a1fa858f6dc57c5c2fc
saitTalhaNisanci <s.talhanisanci@gmail.com>
2018-03-23 15:26:59 +0300

    add orgWebsite samples

---
+++ samples/org_website_samples/query_sample.go
@@ -0,0 +1,114 @@
+// Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org_website_samples
+
+import (
+	"github.com/hazelcast/hazelcast-go-client"
+	. "github.com/hazelcast/hazelcast-go-client/core"
+	"github.com/hazelcast/hazelcast-go-client/serialization"
+	"log"
+)
+
+func querySampleRun() {
+	// Start the Hazelcast Client and connect to an already running Hazelcast Cluster on 127.0.0.1
+	clientConfig := hazelcast.NewHazelcastConfig()
+	clientConfig.SerializationConfig().
+		AddPortableFactory(portableFactoryId, &PortableFactory{})
+	hz, _ := hazelcast.NewHazelcastClientWithConfig(clientConfig)
+	// Get a Distributed Map called "users"
+	users, _ := hz.GetMap("users")
+	// Add some users to the Distributed Map
+	GenerateUsers(users)
+	// Create a Predicate from a String (a SQL like Where clause)
+	var sqlQuery = Sql("active AND age BETWEEN 18 AND 21)")
+	// Creating the same Predicate as above but with a builder
+	var criteriaQuery = And([]serialization.IPredicate{Equal("active", true), Between("age", 18, 21)})
+	// Get result collections using the two different Predicates
+	result1, _ := users.ValuesWithPredicate(sqlQuery)
+	result2, _ := users.ValuesWithPredicate(criteriaQuery)
+	// Print out the results
+	log.Println(result1)
+	log.Println(result2)
+	// Shutdown the Hazelcast Cluster Member
+	hz.Shutdown()
+}
+
+func GenerateUsers(users IMap) {
+	users.Put("Rod", newUser("Rod", 19, true))
+	users.Put("Jane", newUser("Jane", 20, true))
+	users.Put("Freddy", newUser("Freddy", 23, true))
+}
+
+const (
+	userClassId       = 1
+	userFactoryId     = 1
+	portableFactoryId = 1
+)
+
+type User struct {
+	username string
+	age      int32
+	active   bool
+}
+
+func newUser(username string, age int32, active bool) *User {
+	return &User{
+		username: username,
+		age:      age,
+		active:   active,
+	}
+}
+
+func (user *User) FactoryId() int32 {
+	return userFactoryId
+}
+
+func (user *User) ClassId() int32 {
+	return userClassId
+}
+
+func (user *User) WritePortable(writer serialization.PortableWriter) error {
+	writer.WriteUTF("username", user.username)
+	writer.WriteInt32("age", user.age)
+	writer.WriteBool("active", user.active)
+	return nil
+}
+
+func (user *User) ReadPortable(reader serialization.PortableReader) error {
+	var err error
+	user.username, err = reader.ReadUTF("username")
+	if err != nil {
+		return err
+	}
+	user.age, err = reader.ReadInt32("age")
+	if err != nil {
+		return err
+	}
+	user.active, err = reader.ReadBool("active")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type PortableFactory struct {
+}
+
+func (portableFactory *PortableFactory) Create(classId int32) serialization.Portable {
+	if classId == userClassId {
+		return &User{}
+	}
+	return nil
+}
