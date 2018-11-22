8e748c2e935173e69b3c0db973156b5fb4b44d73
saitTalhaNisanci <s.talhanisanci@gmail.com>
2018-11-22 15:21:58 +0300

    Add custom logger test and no logger implementation

---
+++ test/logger/no_logger_test.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logger
+
+import (
+	"bytes"
+	"log"
+	"testing"
+
+	"github.com/hazelcast/hazelcast-go-client"
+	"github.com/hazelcast/hazelcast-go-client/core/logger"
+	"github.com/hazelcast/hazelcast-go-client/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNoLogger(t *testing.T) {
+	cluster, _ := remoteController.CreateCluster("", test.DefaultServerConfig)
+	defer remoteController.ShutdownCluster(cluster.ID)
+	remoteController.StartMember(cluster.ID)
+	buf := new(bytes.Buffer)
+	log.SetOutput(buf)
+	config := hazelcast.NewConfig()
+	noLogger := logger.NewNoLogger()
+	config.LoggerConfig().SetLogger(noLogger)
+	client, err := hazelcast.NewClientWithConfig(config)
+	assert.NoError(t, err)
+	assert.Zero(t, buf.Len())
+	client.Shutdown()
+}
