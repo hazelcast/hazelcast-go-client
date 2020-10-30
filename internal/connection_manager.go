e11bfd990ba6ada526b1c00350e5243414e2fae0
ilkerkorkut <ilker@hazelcast.com>
2020-10-30 13:54:09 +0300

    client build version

--- internal/connection_manager.go
+++ internal/connection_manager.go
@@ -32,7 +32,7 @@ const (
 
 const serializationVersion = 1
 
-var ClientVersion = "0.5-SNAPSHOT" //TODO This should be replace with a build time version variable, BuildInfo etc.
+var ClientVersion string
 
 type connectionManager interface {
 	//getActiveConnections returns a snapshot of active connections
