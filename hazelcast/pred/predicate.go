9298a0faa2e5e32836e0103a3bc279f4e08aa5d5
Yuce Tekol <yucetekol@gmail.com>
2021-04-05 11:46:43 +0300

    trivial cleanup

--- hazelcast/pred/predicate.go
+++ hazelcast/pred/predicate.go
@@ -6,27 +6,6 @@ import (
 	"github.com/hazelcast/hazelcast-go-client/v4/hazelcast/serialization"
 )
 
-const (
-	sqlID = iota
-	andID
-	betweenID
-	equalID
-	greaterlessID
-	likeID
-	ilikeID
-	inID
-	instanceOfID
-	notEqualID
-	notID
-	orID
-	regexID
-	falseID
-	trueID
-	// pagingID
-	// partitionID
-	// nilObjectID
-)
-
 const factoryID = -20
 
 type Predicate interface {
