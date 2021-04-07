583899c9e9868ca15bfbd55a7ed618b905b8556c
Yuce Tekol <yucetekol@gmail.com>
2021-04-07 12:59:01 +0300

    - Added example test - Trivial cleanup

---
+++ hazelcast/hztypes/example_test.go
@@ -0,0 +1,25 @@
+package hztypes_test
+
+import (
+	"log"
+
+	"github.com/hazelcast/hazelcast-go-client/v4/hazelcast"
+)
+
+func Example() {
+	// Start the client.
+	client, err := hazelcast.StartNewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Retrieve a map.
+	peopleMap, err := client.GetMap("people")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Call map functions.
+	err = peopleMap.Set("jane", "doe")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
