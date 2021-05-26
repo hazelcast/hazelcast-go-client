42c1ad61f6e5e20c50c98e8a17ad5a279350d40e
Yuce Tekol <yucetekol@gmail.com>
2021-05-26 18:08:20 +0300

    Added Azure code sample

---
+++ examples/discovery/azure/main.go
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hazelcast/hazelcast-go-client"
+)
+
+func makeKeyValue(i int) (key string, value string) {
+	key = fmt.Sprintf("key-%d", i)
+	value = fmt.Sprintf("value-%d", i)
+	return
+}
+
+func main() {
+	config := hazelcast.NewConfig()
+	config.ClusterConfig.AzureConfig.Enabled = true
+	client, err := hazelcast.StartNewClientWithConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := client.GetMap("sample-map")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < 100; i++ {
+		key, value := makeKeyValue(i)
+		log.Printf("writing %s=%s", key, value)
+		if err = m.Set(key, value); err != nil {
+			log.Fatal(err)
+		}
+	}
+	for i := 0; i < 100; i++ {
+		key, value := makeKeyValue(i)
+		log.Printf("reading %s", key)
+		readValue, err := m.Get(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if value != readValue {
+			log.Printf("unexpected value: %s != %s", value, readValue)
+		}
+	}
+	if err = client.Shutdown(); err != nil {
+		log.Fatal(err)
+	}
+}
