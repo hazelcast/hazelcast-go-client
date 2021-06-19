940f0ebddbd1e1049892518ee165aa7131537dbc
Yuce Tekol <yucetekol@gmail.com>
2021-06-19 17:16:04 +0300

    Noos

--- example_test.go
+++ example_test.go
@@ -1,3 +1,5 @@
+// +build noos
+
 /*
  * Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
  *
@@ -13,8 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-// +build noos
 
 package hazelcast_test
 
