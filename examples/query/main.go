f6bd47bad5836f88de411746dea6b318719da9a2
Martijn de Heus <martijn@hazelcast.com>
2021-09-07 10:31:00 +0200

    Add predicate (query example)

---
+++ examples/query/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/hazelcast/hazelcast-go-client"
+	"github.com/hazelcast/hazelcast-go-client/predicate"
+	"github.com/hazelcast/hazelcast-go-client/serialization"
+)
+
+func main() {
+	// Start the client with defaults
+	ctx := context.TODO()
+	client, err := hazelcast.StartNewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get a random topic
+	rand.Seed(time.Now().Unix())
+	mapName := fmt.Sprintf("sample-%d", rand.Int())
+	m, err := client.GetMap(ctx, mapName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m.Put(ctx, "key-1", serialization.JSON(`{"property: 5}`))
+	m.Put(ctx, "key-2", serialization.JSON(`{"property": 10}`))
+	m.Put(ctx, "key-3", serialization.JSON(`{"property": 15}`))
+
+	// Print all entries in the map
+	entries, err := m.GetEntrySet(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(entries)
+
+	// Filter the entries in the map based on a predicate and print those
+	pred := predicate.And(predicate.Less("property", 12), predicate.Greater("property", 8))
+	entries, err = m.GetEntrySetWithPredicate(ctx, pred)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(entries)
+}
