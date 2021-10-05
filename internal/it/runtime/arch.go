c1d9b9f5cd11f68622bee91950cd9a61576a8ce1
utkucaglayan <utku.caglayan@hazelcast.com>
2021-10-05 09:29:55 +0300

    refactor for loop to switch statement

--- internal/it/runtime/arch.go
+++ internal/it/runtime/arch.go
@@ -3,11 +3,9 @@ package runtime
 import "runtime"
 
 func Is32BitArch() bool {
-	archs := []string{"386", "arm", "mips", "mipsle"}
-	for _, arch := range archs {
-		if runtime.GOARCH == arch {
-			return true
-		}
+	switch runtime.GOARCH {
+	case "386", "arm", "mips", "mipsle":
+		return true
 	}
 	return false
 }
