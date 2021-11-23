9ade89a689477beb2c9b0fb7ca6f5264e487a130
utkucaglayan <utku.caglayan@hazelcast.com>
2021-11-23 13:50:44 +0300

    minor fixes

--- examples/map/main.go
+++ examples/map/main.go
@@ -28,32 +28,32 @@ import (
 )
 
 func main() {
-	// Start the client with defaults
+	// Start the client with defaults.
 	ctx := context.TODO()
 	client, err := hazelcast.StartNewClient(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Get a random map
+	// Get a random map.
 	rand.Seed(time.Now().Unix())
 	mapName := fmt.Sprintf("sample-%d", rand.Int())
 	m, err := client.GetMap(ctx, mapName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Populate map
+	// Populate the map.
 	replacedValue, err := m.Put(ctx, "key", "value")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Get and print result
+	// Get and print result.
 	fmt.Println(replacedValue)
 	value, err := m.Get(ctx, "key")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(value)
-	// Atomic putIfAbsent method
+	// Atomic putIfAbsent method.
 	val, err := m.PutIfAbsent(ctx, "key-2", "value-2")
 	if err != nil {
 		log.Fatal(err)
@@ -61,7 +61,7 @@ func main() {
 	if isNil(val) {
 		fmt.Println("Inserted value")
 	}
-	// Atomic replace if same method
+	// Atomic replace if same method.
 	replaced, err := m.ReplaceIfSame(ctx, "key-2", "value-2", "value3")
 	if err != nil {
 		log.Fatal(err)
@@ -69,7 +69,7 @@ func main() {
 	if replaced {
 		fmt.Println("Replaced value")
 	}
-	// Shutdown client
+	// Shutdown client.
 	client.Shutdown(ctx)
 }
 
--- examples/multimap/main.go
+++ examples/multimap/main.go
@@ -11,38 +11,43 @@ import (
 )
 
 func main() {
-	// Start the client with defaults
+	// Start the client with defaults.
 	ctx := context.TODO()
 	client, err := hazelcast.StartNewClient(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Get a random map
+	// Get a random map.
 	rand.Seed(time.Now().Unix())
 	mapName := fmt.Sprintf("sample-%d", rand.Int())
 	m, err := client.GetMultiMap(ctx, mapName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Populate map
-	if success, err := m.Put(ctx, "key", "value1"); err != nil {
+
+	// Populate the map.
+	success, err := m.Put(ctx, "key", "value1")
+	if err != nil {
 		log.Fatal(err)
-	} else if !success {
+	}
+	if !success {
 		log.Fatal("multi-map put operation failed")
 	}
-	if success, err := m.Put(ctx, "key", "value2"); err != nil {
+	success, err = m.Put(ctx, "key", "value2")
+	if err != nil {
 		log.Fatal(err)
-	} else if !success {
+	}
+	if !success {
 		log.Fatal("multi-map put operation failed")
 	}
 
-	// Get both values under the same key
+	// Get both values under the same key.
 	values, err := m.Get(ctx, "key")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// We see values in an interface slice
+	// An interface slice contains the values.
 	// []interface {}{"value2", "value1"}
 	fmt.Printf("%#v", values)
 }
