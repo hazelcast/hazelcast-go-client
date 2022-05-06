c76df4565452f7c139e403c81552343911b8faae
utkucaglayan <utku.caglayan@hazelcast.com>
2022-05-06 19:09:18 +0300

    add missing API usage

--- examples/sql/client-api/advanced_query_options/main.go
+++ examples/sql/client-api/advanced_query_options/main.go
@@ -18,12 +18,98 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/hazelcast/hazelcast-go-client"
 	"github.com/hazelcast/hazelcast-go-client/sql"
 )
+
+func printStatement(stmt sql.Statement) {
+	var (
+		query            = stmt.SQL()
+		params           = stmt.Parameters()
+		timeout          = stmt.QueryTimeout()
+		schema           = stmt.Schema()
+		cursorBufferSize = stmt.CursorBufferSize()
+	)
+	var expectedResultType string
+	switch stmt.ExpectedResultType() {
+	case sql.ExpectedResultTypeAny:
+		expectedResultType = "Any"
+	case sql.ExpectedResultTypeRows:
+		expectedResultType = "Rows"
+	case sql.ExpectedResultTypeUpdateCount:
+		expectedResultType = "Update Count"
+	}
+	fmt.Printf(`Stamement Details:
+	Query String: %s
+	Params: %+v
+	Query Timeout: %d ms
+	Schema: "%s"
+	Expected Result Type: %s
+	Cursor Buffer Size: %d`+"\n\n", query, params, timeout, schema, expectedResultType, cursorBufferSize)
+}
+
+func printResultDetails(r sql.Result) error {
+	if r.IsRowSet() {
+		rm, err := r.RowMetadata()
+		if err != nil {
+			return err
+		}
+		colCount := rm.ColumnCount()
+		columns := rm.Columns()
+		fmt.Printf("Row Result [%d columns]\n", colCount)
+		for i, col := range columns {
+			fmt.Printf(`	Column %d:
+		Name: %s
+		Type: %d
+		Nullable: %t`+"\n", i, col.Name(), col.Type(), col.Nullable())
+		}
+	} else {
+		fmt.Printf("Update Result [%d rows updated]\n", r.UpdateCount())
+	}
+	return nil
+}
+
+func executeAndPrintDetails(ctx context.Context, service sql.Service, stmt sql.Statement) (sql.Result, error) {
+	printStatement(stmt)
+	result, err := service.ExecuteStatement(ctx, stmt)
+	if err != nil {
+		var sqlError *sql.Error
+		if errors.As(err, &sqlError) {
+			err = fmt.Errorf(`SQLErr:
+	Message: %s
+	Suggestion: %s
+	MemberID: %s
+	ErrCode: %d`+"\n", sqlError.Message, sqlError.Suggestion, sqlError.OriginatingMemberId, sqlError.Code)
+		}
+		return nil, err
+	}
+	return result, printResultDetails(result)
+}
+
+func sinkValues(ctx context.Context, sqlService sql.Service, stmt sql.Statement) error {
+	// set statement string and result type to reuse on iterations
+	if err := stmt.SetSQL(`SINK INTO mymap VALUES(?, ?)`); err != nil {
+		return fmt.Errorf("setting query string: %w", err)
+	}
+	// setting expected result type helps cluster to save time planning the statement execution
+	if err := stmt.SetExpectedResultType(sql.ExpectedResultTypeUpdateCount); err != nil {
+		return fmt.Errorf("setting result type: %w", err)
+	}
+	for i := 0; i < 100; i++ {
+		// change params on every iteration
+		stmt.SetParameters(i, fmt.Sprintf("sample-string-%d", i))
+		result, err := executeAndPrintDetails(ctx, sqlService, stmt)
+		if err != nil {
+			return err
+		}
+		result.Close()
+	}
+	return nil
+}
 
 func main() {
 	ctx := context.Background()
@@ -42,22 +128,26 @@ func main() {
 	`)
 	stmt.SetSchema("partitioned")
 	sqlService := client.SQL()
-	_, err = sqlService.ExecuteStatement(ctx, stmt)
+	r, err := executeAndPrintDetails(ctx, sqlService, stmt)
 	if err != nil {
 		panic(fmt.Errorf("creating the mapping: %w", err))
 	}
-	for i := 0; i < 100; i++ {
-		if _, err = sqlService.Execute(ctx, `SINK INTO mymap VALUES(?, ?)`,
-			i, fmt.Sprintf("sample string-%d", i)); err != nil {
-			panic(fmt.Errorf("inserting values: %w", err))
-		}
+	r.Close()
+	if err = sinkValues(ctx, sqlService, stmt); err != nil {
+		panic(fmt.Errorf("inserting values: %w", err))
 	}
-	stmt = sql.NewStatement(`SELECT __key, this from mymap order by __key`)
+	// reset statement to reuse for following queries
+	stmt.ClearParameters()
 	stmt.SetQueryTimeout(5 * time.Second)
-	stmt.SetCursorBufferSize(10)
-	result, err := sqlService.ExecuteStatement(ctx, stmt)
+	if err := stmt.SetCursorBufferSize(10); err != nil {
+		panic(fmt.Errorf("setting cursor buffer size: %w", err))
+	}
+	if err := stmt.SetSQL(`SELECT * from mymap order by __key`); err != nil {
+		panic(fmt.Errorf("setting query string: %w", err))
+	}
+	result, err := executeAndPrintDetails(ctx, sqlService, stmt)
 	if err != nil {
-		panic(fmt.Errorf("querying: %w", err))
+		panic(fmt.Errorf("query map: %w", err))
 	}
 	defer result.Close()
 	it, err := result.Iterator()
@@ -69,14 +159,20 @@ func main() {
 		if err != nil {
 			panic(fmt.Errorf("iterating rows: %w", err))
 		}
-		key, err := row.Get(0)
+		// we can access the value by column name
+		key, err := row.GetByColumnName("__key")
 		if err != nil {
 			panic(fmt.Errorf("accessing row: %w", err))
 		}
-		value, err := row.Get(1)
+		// or we can access it via index
+		index, err := row.Metadata().FindColumn("this")
 		if err != nil {
 			panic(fmt.Errorf("accessing row: %w", err))
 		}
-		fmt.Printf("--> %d: %s\n", key, value)
+		value, err := row.Get(index)
+		if err != nil {
+			panic(fmt.Errorf("accessing row: %w", err))
+		}
+		fmt.Printf("Key %d --> %s\n", key, value)
 	}
 }
