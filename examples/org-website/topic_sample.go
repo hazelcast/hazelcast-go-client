5212fde919f5bf3f7338e3b9d06df00ae148d19f
Yuce Tekol <yuce.tekol@hazelcast.com>
2022-05-13 05:50:41 +0300

    updated the topic sample

--- examples/org-website/topic_sample.go
+++ examples/org-website/topic_sample.go
@@ -1,48 +1,60 @@
+//go:build ignore
 // +build ignore
+
+/*
+ * Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package main
 
 import (
 	"context"
 	"fmt"
-	"log"
-	"math/rand"
-	"time"
 
 	"github.com/hazelcast/hazelcast-go-client"
 )
 
-// messageListener handles incoming messages to the topic
+// messageListener handles incoming messages to the topic.
 func messageListener(event *hazelcast.MessagePublished) {
 	fmt.Println("Received message: ", event.Value)
 }
 
 func main() {
-	// Start the client with defaults
 	ctx := context.TODO()
+	// Start the client with defaults.
 	client, err := hazelcast.StartNewClient(ctx)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	// Get a random topic
-	rand.Seed(time.Now().Unix())
-	topicName := fmt.Sprintf("sample-%d", rand.Int())
-	topic, err := client.GetTopic(ctx, topicName)
+	// Get a reference to the queue.
+	myTopic, err := client.GetTopic(ctx, "my-topic")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	// Add a message listener to the topic
-	_, err = topic.AddMessageListener(ctx, messageListener)
+	// Add a message listener to the topic.
+	_, err = myTopic.AddMessageListener(ctx, messageListener)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	// Publish messages to topic
 	for i := 0; i < 10; i++ {
-		err = topic.Publish(ctx, fmt.Sprintf("Message %d", i))
+		err = myTopic.Publish(ctx, fmt.Sprintf("Message %d", i))
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 	}
-	// Shutdown client
+	// Shutdown the client.
 	client.Shutdown(ctx)
 }
