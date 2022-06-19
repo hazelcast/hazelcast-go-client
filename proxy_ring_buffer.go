ff4bf6521c968ebf1ce850a4ef29a818c7c156e7
nitram509 <maki@bitkings.de>
2022-06-19 22:48:10 +0200

    dropped mention of 'collection' cause Java related (#674)

--- proxy_ring_buffer.go
+++ proxy_ring_buffer.go
@@ -120,10 +120,10 @@ func (rb *Ringbuffer) Add(ctx context.Context, item interface{}, overflowPolicy 
 // AddAll
 // Adds all the items of a collection to the tail of the Ringbuffer. A addAll is likely to outperform multiple calls
 // to add(Object) due to better io utilization and a reduced number of executed operations. If the batch is empty,
-// the call is ignored. When the collection is not empty, the content is copied into a different data-structure.
-// This means that: after this call completes, the collection can be re-used. the collection doesn't need to be serializable.
-// If the collection is larger than the capacity of the Ringbuffer, then the items that were written first will be
-// overwritten. Therefore, this call will not block. The items are inserted in the order of the Iterator of the collection.
+// the call is ignored. When the slice is not empty, the content is copied into a different data-structure.
+// This means that: after this call completes, the slice can be re-used.
+// If the slice is larger than the capacity of the Ringbuffer, then the items that were written first will be
+// overwritten. Therefore, this call will not block. The items are inserted in the order of the slice.
 // If an addAll is executed concurrently with an add or addAll, no guarantee is given that items are contiguous.
 // The result contains the sequenceId of the last written item.
 func (rb *Ringbuffer) AddAll(ctx context.Context, overflowPolicy OverflowPolicy, items ...interface{}) (int64, error) {
