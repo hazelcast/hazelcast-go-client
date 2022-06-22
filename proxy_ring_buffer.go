604359f2b718a2d71cd19e1852ce0c5c83b4408b
nitram509 <maki@bitkings.de>
2022-06-22 22:21:37 +0200

    move type and consts closer together, part 2 (#674)

--- proxy_ring_buffer.go
+++ proxy_ring_buffer.go
@@ -43,6 +43,9 @@ type ReadResultSet struct {
 	nextSeq          int64
 }
 
+// ReadResultSetSequenceUnavailable is used when error happened
+const ReadResultSetSequenceUnavailable int64 = -1
+
 // OverflowPolicy
 // Using this OverflowPolicy one can control the behavior what should to be done
 // when an item is about to be added to the Ringbuffer, but there is {@code 0}
@@ -52,35 +55,30 @@ type ReadResultSet struct {
 // the Ringbuffer (the head) is not old enough to expire.
 type OverflowPolicy int
 
-const (
-	// OverflowPolicyOverwrite
-	// Using this OverflowPolicyOverwrite policy the oldest item is overwritten
-	// no matter it is not old enough to retire. Using this policy you are
-	// sacrificing the time-to-live in favor of being able to write.
-	//
-	// Example: if there is a time-to-live of 30 seconds, the buffer is full
-	// and the oldest item in the ring has been placed a second ago, then there
-	// are 29 seconds remaining for that item. Using this policy you are going
-	// to overwrite no matter what.
-	OverflowPolicyOverwrite OverflowPolicy = 0
+// OverflowPolicyOverwrite
+// Using this OverflowPolicyOverwrite policy the oldest item is overwritten
+// no matter it is not old enough to retire. Using this policy you are
+// sacrificing the time-to-live in favor of being able to write.
+//
+// Example: if there is a time-to-live of 30 seconds, the buffer is full
+// and the oldest item in the ring has been placed a second ago, then there
+// are 29 seconds remaining for that item. Using this policy you are going
+// to overwrite no matter what.
+const OverflowPolicyOverwrite OverflowPolicy = 0
 
-	// OverflowPolicyFail
-	// Using this policy the call will fail immediately and the oldest item will
-	// not be overwritten before it is old enough to retire. So this policy
-	// sacrificing the ability to write in favor of time-to-live.
-	//
-	// The advantage of OverflowPolicyFail is that the caller can decide what to do
-	// since it doesn't trap the thread due to backoff.
-	//
-	// Example: if there is a time-to-live of 30 seconds, the buffer is full
-	// and the oldest item in the ring has been placed a second ago, then there
-	// are 29 seconds remaining for that item. Using this policy you are not
-	// going to overwrite that item for the next 29 seconds.
-	OverflowPolicyFail OverflowPolicy = 1
-
-	// ReadResultSetSequenceUnavailable is used when error happened
-	ReadResultSetSequenceUnavailable int64 = -1
-)
+// OverflowPolicyFail
+// Using this policy the call will fail immediately and the oldest item will
+// not be overwritten before it is old enough to retire. So this policy
+// sacrificing the ability to write in favor of time-to-live.
+//
+// The advantage of OverflowPolicyFail is that the caller can decide what to do
+// since it doesn't trap the thread due to backoff.
+//
+// Example: if there is a time-to-live of 30 seconds, the buffer is full
+// and the oldest item in the ring has been placed a second ago, then there
+// are 29 seconds remaining for that item. Using this policy you are not
+// going to overwrite that item for the next 29 seconds.
+const OverflowPolicyFail OverflowPolicy = 1
 
 func newRingbuffer(p *proxy) (*Ringbuffer, error) {
 	partitionID, err := p.stringToPartitionID(p.name)
