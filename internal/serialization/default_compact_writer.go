e5148115d142858a568f4f27ab3bb7693269ecd5
Serkan Özel <serkan.ozel@hazelcast.com>
2022-06-24 11:01:56 +0300

    Make defcompactreader/writer pointer receiver

--- internal/serialization/default_compact_writer.go
+++ internal/serialization/default_compact_writer.go
@@ -35,7 +35,7 @@ type DefaultCompactWriter struct {
 	dataStartPos int32
 }
 
-func NewDefaultCompactWriter(serializer CompactStreamSerializer, out *PositionalObjectDataOutput, schema Schema) DefaultCompactWriter {
+func NewDefaultCompactWriter(serializer CompactStreamSerializer, out *PositionalObjectDataOutput, schema Schema) *DefaultCompactWriter {
 	var fieldOffsets []int32
 	var dataStartPosition int32
 	if schema.numberOfVarSizeFields != 0 {
@@ -49,7 +49,7 @@ func NewDefaultCompactWriter(serializer CompactStreamSerializer, out *Positional
 		// variable-size fields.
 		out.WriteZeroBytes(int(schema.fixedSizeFieldsLength))
 	}
-	return DefaultCompactWriter{
+	return &DefaultCompactWriter{
 		serializer:   serializer,
 		out:          out,
 		schema:       schema,
@@ -58,7 +58,7 @@ func NewDefaultCompactWriter(serializer CompactStreamSerializer, out *Positional
 	}
 }
 
-func (r DefaultCompactWriter) WriteBoolean(fieldName string, value bool) {
+func (r *DefaultCompactWriter) WriteBoolean(fieldName string, value bool) {
 	fd := r.getFieldDescriptorChecked(fieldName, pubserialization.FieldKindBoolean)
 	offsetInBytes := fd.offset
 	offsetInBits := fd.bitOffset
@@ -66,248 +66,248 @@ func (r DefaultCompactWriter) WriteBoolean(fieldName string, value bool) {
 	r.out.PWriteBoolBit(writeOffset, int32(offsetInBits), value)
 }
 
-func (r DefaultCompactWriter) WriteInt8(fieldName string, value int8) {
+func (r *DefaultCompactWriter) WriteInt8(fieldName string, value int8) {
 	position := r.getFixedSizeFieldPosition(fieldName, pubserialization.FieldKindInt8)
 	r.out.PWriteByte(position, byte(value))
 }
 
-func (r DefaultCompactWriter) WriteInt16(fieldName string, value int16) {
+func (r *DefaultCompactWriter) WriteInt16(fieldName string, value int16) {
 	position := r.getFixedSizeFieldPosition(fieldName, pubserialization.FieldKindInt16)
 	r.out.PWriteInt16(position, value)
 }
 
-func (r DefaultCompactWriter) WriteInt32(fieldName string, value int32) {
+func (r *DefaultCompactWriter) WriteInt32(fieldName string, value int32) {
 	position := r.getFixedSizeFieldPosition(fieldName, pubserialization.FieldKindInt32)
 	r.out.PWriteInt32(position, value)
 }
 
-func (r DefaultCompactWriter) WriteInt64(fieldName string, value int64) {
+func (r *DefaultCompactWriter) WriteInt64(fieldName string, value int64) {
 	position := r.getFixedSizeFieldPosition(fieldName, pubserialization.FieldKindInt64)
 	r.out.PWriteInt64(position, value)
 }
 
-func (r DefaultCompactWriter) WriteFloat32(fieldName string, value float32) {
+func (r *DefaultCompactWriter) WriteFloat32(fieldName string, value float32) {
 	position := r.getFixedSizeFieldPosition(fieldName, pubserialization.FieldKindFloat32)
 	r.out.PWriteFloat32(position, value)
 }
 
-func (r DefaultCompactWriter) WriteFloat64(fieldName string, value float64) {
+func (r *DefaultCompactWriter) WriteFloat64(fieldName string, value float64) {
 	position := r.getFixedSizeFieldPosition(fieldName, pubserialization.FieldKindFloat64)
 	r.out.PWriteFloat64(position, value)
 }
 
-func (r DefaultCompactWriter) WriteString(fieldName string, value *string) {
+func (r *DefaultCompactWriter) WriteString(fieldName string, value *string) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindString, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteString(*v.(*string))
 	})
 }
 
-func (r DefaultCompactWriter) WriteDecimal(fieldName string, value *types.Decimal) {
+func (r *DefaultCompactWriter) WriteDecimal(fieldName string, value *types.Decimal) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindDecimal, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteDecimal(out, *v.(*types.Decimal))
 	})
 }
 
-func (r DefaultCompactWriter) WriteTime(fieldName string, value *types.LocalTime) {
+func (r *DefaultCompactWriter) WriteTime(fieldName string, value *types.LocalTime) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindTime, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteTime(out, time.Time(*v.(*types.LocalTime)))
 	})
 }
 
-func (r DefaultCompactWriter) WriteDate(fieldName string, value *types.LocalDate) {
+func (r *DefaultCompactWriter) WriteDate(fieldName string, value *types.LocalDate) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindDate, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteDate(out, time.Time(*v.(*types.LocalDate)))
 	})
 }
 
-func (r DefaultCompactWriter) WriteTimestamp(fieldName string, value *types.LocalDateTime) {
+func (r *DefaultCompactWriter) WriteTimestamp(fieldName string, value *types.LocalDateTime) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindTimestamp, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteTimestamp(out, time.Time(*v.(*types.LocalDateTime)))
 	})
 }
 
-func (r DefaultCompactWriter) WriteTimestampWithTimezone(fieldName string, value *types.OffsetDateTime) {
+func (r *DefaultCompactWriter) WriteTimestampWithTimezone(fieldName string, value *types.OffsetDateTime) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindTimestampWithTimezone, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteTimestampWithTimezone(out, time.Time(*v.(*types.OffsetDateTime)))
 	})
 }
 
-func (r DefaultCompactWriter) WriteCompact(fieldName string, value interface{}) {
+func (r *DefaultCompactWriter) WriteCompact(fieldName string, value interface{}) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindCompact, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		r.serializer.Write(out, reflect.ValueOf(v).Elem().Interface())
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfBoolean(fieldName string, value []bool) {
+func (r *DefaultCompactWriter) WriteArrayOfBoolean(fieldName string, value []bool) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindArrayOfBoolean, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		r.writeBooleanBits(out, v.([]bool))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfInt8(fieldName string, value []int8) {
+func (r *DefaultCompactWriter) WriteArrayOfInt8(fieldName string, value []int8) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindArrayOfInt8, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt8Array(v.([]int8))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfInt16(fieldName string, value []int16) {
+func (r *DefaultCompactWriter) WriteArrayOfInt16(fieldName string, value []int16) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindArrayOfInt16, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt16Array(v.([]int16))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfInt32(fieldName string, value []int32) {
+func (r *DefaultCompactWriter) WriteArrayOfInt32(fieldName string, value []int32) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindArrayOfInt32, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt32Array(v.([]int32))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfInt64(fieldName string, value []int64) {
+func (r *DefaultCompactWriter) WriteArrayOfInt64(fieldName string, value []int64) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindArrayOfInt64, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt64Array(v.([]int64))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfFloat32(fieldName string, value []float32) {
+func (r *DefaultCompactWriter) WriteArrayOfFloat32(fieldName string, value []float32) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindArrayOfFloat32, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteFloat32Array(v.([]float32))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfFloat64(fieldName string, value []float64) {
+func (r *DefaultCompactWriter) WriteArrayOfFloat64(fieldName string, value []float64) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindArrayOfFloat64, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteFloat64Array(v.([]float64))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfString(fieldName string, value []*string) {
+func (r *DefaultCompactWriter) WriteArrayOfString(fieldName string, value []*string) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfString, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteString(*v.(*string))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfDecimal(fieldName string, value []*types.Decimal) {
+func (r *DefaultCompactWriter) WriteArrayOfDecimal(fieldName string, value []*types.Decimal) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfDecimal, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteDecimal(out, *v.(*types.Decimal))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfTime(fieldName string, value []*types.LocalTime) {
+func (r *DefaultCompactWriter) WriteArrayOfTime(fieldName string, value []*types.LocalTime) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfTime, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteTime(out, time.Time(*v.(*types.LocalTime)))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfDate(fieldName string, value []*types.LocalDate) {
+func (r *DefaultCompactWriter) WriteArrayOfDate(fieldName string, value []*types.LocalDate) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfDate, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteDate(out, time.Time(*v.(*types.LocalDate)))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfTimestamp(fieldName string, value []*types.LocalDateTime) {
+func (r *DefaultCompactWriter) WriteArrayOfTimestamp(fieldName string, value []*types.LocalDateTime) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfTimestamp, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteTimestamp(out, time.Time(*v.(*types.LocalDateTime)))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfTimestampWithTimezone(fieldName string, value []*types.OffsetDateTime) {
+func (r *DefaultCompactWriter) WriteArrayOfTimestampWithTimezone(fieldName string, value []*types.OffsetDateTime) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfTimestampWithTimezone, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		WriteTimestampWithTimezone(out, time.Time(*v.(*types.OffsetDateTime)))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfCompact(fieldName string, value []interface{}) {
+func (r *DefaultCompactWriter) WriteArrayOfCompact(fieldName string, value []interface{}) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfCompact, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		r.serializer.Write(out, reflect.ValueOf(v).Elem().Interface())
 	})
 }
 
-func (r DefaultCompactWriter) WriteNullableBoolean(fieldName string, value *bool) {
+func (r *DefaultCompactWriter) WriteNullableBoolean(fieldName string, value *bool) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindNullableBoolean, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteBool(*v.(*bool))
 	})
 }
 
-func (r DefaultCompactWriter) WriteNullableInt8(fieldName string, value *int8) {
+func (r *DefaultCompactWriter) WriteNullableInt8(fieldName string, value *int8) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindNullableInt8, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteSignedByte(*v.(*int8))
 	})
 }
 
-func (r DefaultCompactWriter) WriteNullableInt16(fieldName string, value *int16) {
+func (r *DefaultCompactWriter) WriteNullableInt16(fieldName string, value *int16) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindNullableInt16, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt16(*v.(*int16))
 	})
 }
 
-func (r DefaultCompactWriter) WriteNullableInt32(fieldName string, value *int32) {
+func (r *DefaultCompactWriter) WriteNullableInt32(fieldName string, value *int32) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindNullableInt32, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt32(*v.(*int32))
 	})
 }
 
-func (r DefaultCompactWriter) WriteNullableInt64(fieldName string, value *int64) {
+func (r *DefaultCompactWriter) WriteNullableInt64(fieldName string, value *int64) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindNullableInt64, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt64(*v.(*int64))
 	})
 }
 
-func (r DefaultCompactWriter) WriteNullableFloat32(fieldName string, value *float32) {
+func (r *DefaultCompactWriter) WriteNullableFloat32(fieldName string, value *float32) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindNullableFloat32, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteFloat32(*v.(*float32))
 	})
 }
 
-func (r DefaultCompactWriter) WriteNullableFloat64(fieldName string, value *float64) {
+func (r *DefaultCompactWriter) WriteNullableFloat64(fieldName string, value *float64) {
 	r.writeVariableSizeField(fieldName, pubserialization.FieldKindNullableFloat64, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteFloat64(*v.(*float64))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfNullableBoolean(fieldName string, value []*bool) {
+func (r *DefaultCompactWriter) WriteArrayOfNullableBoolean(fieldName string, value []*bool) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfNullableBoolean, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteBool(*v.(*bool))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfNullableInt8(fieldName string, value []*int8) {
+func (r *DefaultCompactWriter) WriteArrayOfNullableInt8(fieldName string, value []*int8) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfNullableInt8, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteSignedByte(*v.(*int8))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfNullableInt16(fieldName string, value []*int16) {
+func (r *DefaultCompactWriter) WriteArrayOfNullableInt16(fieldName string, value []*int16) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfNullableInt16, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt16(*v.(*int16))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfNullableInt32(fieldName string, value []*int32) {
+func (r *DefaultCompactWriter) WriteArrayOfNullableInt32(fieldName string, value []*int32) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfNullableInt32, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt32(*v.(*int32))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfNullableInt64(fieldName string, value []*int64) {
+func (r *DefaultCompactWriter) WriteArrayOfNullableInt64(fieldName string, value []*int64) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfNullableInt64, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteInt64(*v.(*int64))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfNullableFloat32(fieldName string, value []*float32) {
+func (r *DefaultCompactWriter) WriteArrayOfNullableFloat32(fieldName string, value []*float32) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfNullableFloat32, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteFloat32(*v.(*float32))
 	})
 }
 
-func (r DefaultCompactWriter) WriteArrayOfNullableFloat64(fieldName string, value []*float64) {
+func (r *DefaultCompactWriter) WriteArrayOfNullableFloat64(fieldName string, value []*float64) {
 	r.writeArrayOfVariableSize(fieldName, pubserialization.FieldKindArrayOfNullableFloat64, value, func(out *PositionalObjectDataOutput, v interface{}) {
 		out.WriteFloat64(*v.(*float64))
 	})
 }
 
 // End ends the serialization of the compact objects by writing the offsets of the variable-size fields as well as the data length, if there are some variable-size fields.
-func (r DefaultCompactWriter) End() {
+func (r *DefaultCompactWriter) End() {
 	if r.schema.numberOfVarSizeFields == 0 {
 		return
 	}
@@ -384,7 +384,7 @@ func (r *DefaultCompactWriter) writeArrayOfVariableSize(fieldName string, fieldK
 	r.writeOffsets(dataLength, offsets)
 }
 
-func (r DefaultCompactWriter) writeOffsets(dataLength int32, fieldOffsets []int32) {
+func (r *DefaultCompactWriter) writeOffsets(dataLength int32, fieldOffsets []int32) {
 	if dataLength < byteOffsetReaderRange {
 		for _, offset := range fieldOffsets {
 			r.out.WriteByte(byte(offset))
@@ -400,7 +400,7 @@ func (r DefaultCompactWriter) writeOffsets(dataLength int32, fieldOffsets []int3
 	}
 }
 
-func (r DefaultCompactWriter) writeBooleanBits(out *PositionalObjectDataOutput, booleans []bool) {
+func (r *DefaultCompactWriter) writeBooleanBits(out *PositionalObjectDataOutput, booleans []bool) {
 	var length int
 	if booleans == nil {
 		length = nilArrayLength
