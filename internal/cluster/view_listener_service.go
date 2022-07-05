bbfe339e55c15a4c26ca82631da17f828c6dea89
Yüce Tekol <yuce.tekol@hazelcast.com>
2022-07-05 10:22:00 +0300

    Remove MakeSubscriptionID method (#815)
    Removed MakeSubscriptionID method

--- internal/cluster/view_listener_service.go
+++ internal/cluster/view_listener_service.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cluster
 
 import (
@@ -8,6 +24,8 @@ import (
 	"github.com/hazelcast/hazelcast-go-client/internal/event"
 	"github.com/hazelcast/hazelcast-go-client/internal/logger"
 )
+
+var viewListenerConnectionEventSubID = event.NextSubscriptionID()
 
 type ViewListenerService struct {
 	connID     int64 // This field should be at the top: https://pkg.go.dev/sync/atomic#pkg-note-BUG
@@ -24,7 +42,7 @@ func NewViewListenerService(cs *Service, cm *ConnectionManager, dispatcher *even
 		dispatcher: dispatcher,
 		logger:     logger,
 	}
-	dispatcher.Subscribe(EventConnection, event.DefaultSubscriptionID, vs.handleConnectionEvent)
+	dispatcher.Subscribe(EventConnection, viewListenerConnectionEventSubID, vs.handleConnectionEvent)
 	return vs
 }
 
