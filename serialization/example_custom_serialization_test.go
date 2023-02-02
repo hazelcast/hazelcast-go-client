42df212a401b067dfddf910b1a3c2f8f8691e813
Yüce Tekol <yuce.tekol@hazelcast.com>
2023-02-02 14:31:04 +0300

    Fix code sample for the custom serialization code sample and documentation (#915)
    Fix code sample for the custom serialization code sample and documentation
    # Conflicts:
    #	internal/serialization/serialization_test.go

--- serialization/example_custom_serialization_test.go
+++ serialization/example_custom_serialization_test.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package serialization_test
 
 import (
@@ -22,43 +38,35 @@ func (e EmployeeCustomSerializer) ID() int32 {
 
 func (e EmployeeCustomSerializer) Read(input serialization.DataInput) interface{} {
 	surname := input.ReadString()
-	return &Employee{Surname: surname}
+	return Employee{Surname: surname}
 }
 
 func (e EmployeeCustomSerializer) Write(output serialization.DataOutput, object interface{}) {
-	employee, ok := object.(*Employee)
-	if !ok {
-		panic("can serialize only Employee")
-	}
-	output.WriteString(employee.Surname)
+	v := object.(Employee)
+	output.WriteString(v.Surname)
 }
 
 func ExampleConfig_SetCustomSerializer() {
 	// Configure serializer
 	config := hazelcast.NewConfig()
-	config.Serialization.SetCustomSerializer(reflect.TypeOf(&Employee{}), &EmployeeCustomSerializer{})
-	// Start the client with custom serializer
+	if err := config.Serialization.SetCustomSerializer(reflect.TypeOf(&Employee{}), &EmployeeCustomSerializer{}); err != nil {
+		panic(err)
+	}
 	ctx := context.TODO()
 	client, err := hazelcast.StartNewClientWithConfig(ctx, config)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Get a random map
 	m, err := client.GetMap(ctx, "map-1")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Store an object in the map
-	_, err = m.Put(ctx, "employee-1", Employee{"Doe"})
-	if err != nil {
-		log.Fatal(err)
+	if _, err := m.Put(ctx, "employee-1", Employee{"Doe"}); err != nil {
+		panic(err)
 	}
-	// Retrieve the object and print
 	value, err := m.Get(ctx, "employee-1")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	fmt.Println("Surname of stored employee:", value.(Employee).Surname)
-	// Shutdown client
-	client.Shutdown(ctx)
 }
