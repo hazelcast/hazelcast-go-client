1f0464ed6e7e216255ddfb743a9b711dc10074a8
Mehmet Tokgöz <56408993+mehmettokgoz@users.noreply.github.com>
2023-03-01 12:57:52 +0300

    Add TLS/SSL code samples and documentation. [API-1837] (#920)

---
+++ examples/ssl/simple/main.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"github.com/hazelcast/hazelcast-go-client"
+)
+
+func main() {
+	// Hazelcast server should be started with SSL enabled to use SSLConfig
+	ctx := context.TODO()
+	var cfg hazelcast.Config
+	cfg = hazelcast.Config{}
+	cfg.Cluster.Network.SetAddresses("foo.bar.com:8888")
+	// TLS/SSL is enabled
+	cfg.Cluster.Network.SSL.Enabled = true
+	// Absolute paths of PEM files must be given
+	err := cfg.Cluster.Network.SSL.SetCAPath("/path/of/server.pem")
+	if err != nil {
+		return
+	}
+	// Set the server name and select the protocol used in SSL communication, default is TLSv1_2
+	cfg.Cluster.Network.SSL.SetTLSConfig(&tls.Config{ServerName: "foo.bar", MinVersion: tls.VersionTLS13})
+	// Start a new Hazelcast client with SSL configuration.
+	client, err := hazelcast.StartNewClientWithConfig(ctx, cfg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connection Successful!")
+	client.Shutdown(ctx)
+}
