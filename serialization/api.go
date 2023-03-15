a082e07ef59cea79d0dbb62d58422a8a9ba05c24
Yüce Tekol <yuce.tekol@hazelcast.com>
2023-03-15 16:03:48 +0300

    Added compact serialization docs [API-1345] (#946)
    Added compact serialization docs

--- serialization/api.go
+++ serialization/api.go
@@ -407,7 +407,7 @@ type PortableReader interface {
 	// ReadDecimal reads a decimal.
 	// It may return nil.
 	ReadDecimal(fieldName string) (d *types.Decimal)
-	// ReadDecimalArray a decimal array.
+	// ReadDecimalArray reads a decimal array.
 	ReadDecimalArray(fieldName string) (ds []types.Decimal)
 }
 
@@ -463,100 +463,244 @@ const (
 	FieldKindArrayOfNullableFloat64       FieldKind = 46
 )
 
+// CompactSerializer must be implemented to serialize a value with compact serialization.
 type CompactSerializer interface {
+	// Type returns the target type for this serializer.
 	Type() reflect.Type
+	/*
+		TypeName returns a string which uniquely identifies this serializers.
+		Choosing a type name will associate that name with the schema and will make the polyglot use cases, where there are multiple clients from different languages, possible.
+		Serializers in different languages can work on the same data, provided that their read and write methods are compatible, and they have the same type name.
+		If you evolve your class in the later versions of your application, by adding or removing fields, you should continue using the same type name for that class.
+	*/
 	TypeName() string
+	// Read reads the fields from reader and creates a value of the type returned from Type()
 	Read(reader CompactReader) interface{}
+	// Write writes the fields of value using the writer.
 	Write(writer CompactWriter, value interface{})
 }
 
+// CompactReader is an interface implemented by types passed to CompactSerializer.Read method.
 type CompactReader interface {
+	// ReadBoolean reads and returns a boolean.
 	ReadBoolean(fieldName string) bool
+	// ReadInt8 reads and returns an int8.
 	ReadInt8(fieldName string) int8
+	// ReadInt16 reads and returns and int16
 	ReadInt16(fieldName string) int16
+	// ReadInt32 reads and returns an int32
 	ReadInt32(fieldName string) int32
+	// ReadInt64 reads and returns an int64
 	ReadInt64(fieldName string) int64
+	// ReadFloat32 reads and returns a float32
 	ReadFloat32(fieldName string) float32
+	// ReadFloat64 reads and returns a float64
 	ReadFloat64(fieldName string) float64
+	// ReadString reads and returns a string which may be nil.
 	ReadString(fieldName string) *string
+	// ReadDecimal reads and returns a *types.Decimal value which may be nil.
 	ReadDecimal(fieldName string) *types.Decimal
+	// ReadTime reads and returns a *types.LocalTime value which may be nil.
 	ReadTime(fieldName string) *types.LocalTime
+	// ReadDate reads and returns a *types.LocalDate value which may be nil.
 	ReadDate(fieldName string) *types.LocalDate
+	// ReadTimestamp reads and returns a *types.LocalDateTime value which may be nil.
 	ReadTimestamp(fieldName string) *types.LocalDateTime
+	// ReadTimestampWithTimezone reads and returns a *types.OffsetDateTime value which may be nil.
 	ReadTimestampWithTimezone(fieldName string) *types.OffsetDateTime
+	// ReadCompact reads and returns a compact serialized value which may be nil.
 	ReadCompact(fieldName string) interface{}
+	// ReadArrayOfBoolean reads and returns a slice of bools.
 	ReadArrayOfBoolean(fieldName string) []bool
+	// ReadArrayOfInt8 reads and returns a slice of int8s.
 	ReadArrayOfInt8(fieldName string) []int8
+	// ReadArrayOfInt16 reads and returns a slice of int16s.
 	ReadArrayOfInt16(fieldName string) []int16
+	// ReadArrayOfInt32 reads and returns a slice of int32s.
 	ReadArrayOfInt32(fieldName string) []int32
+	// ReadArrayOfInt64 reads and returns a slice of int64s.
 	ReadArrayOfInt64(fieldName string) []int64
+	// ReadArrayOfFloat32 reads and returns a slice of float32s.
 	ReadArrayOfFloat32(fieldName string) []float32
+	// ReadArrayOfFloat64 reads and returns a slice of float64s.
 	ReadArrayOfFloat64(fieldName string) []float64
+	// ReadArrayOfString reads and returns a slice of string pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfString(fieldName string) []*string
+	// ReadArrayOfDecimal reads and returns a slice of type.Decimal pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfDecimal(fieldName string) []*types.Decimal
+	// ReadArrayOfTime reads and returns a slice of type.LocalTime pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfTime(fieldName string) []*types.LocalTime
+	// ReadArrayOfDate reads and returns a slice of type.LocalDate pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfDate(fieldName string) []*types.LocalDate
+	// ReadArrayOfTimestamp reads and returns a slice of type.LocalDateTime pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfTimestamp(fieldName string) []*types.LocalDateTime
+	// ReadArrayOfTimestampWithTimezone reads and returns a slice of type.OffsetDateTime pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfTimestampWithTimezone(fieldName string) []*types.OffsetDateTime
+	// ReadArrayOfCompact reads and returns a slice of values.
 	ReadArrayOfCompact(fieldName string) []interface{}
+	// ReadNullableBoolean reads and returns a bool pointer.
+	// The returned pointer may be nil.
 	ReadNullableBoolean(fieldName string) *bool
+	// ReadNullableInt8 reads and returns an int8 pointer.
+	// The returned pointer may be nil.
 	ReadNullableInt8(fieldName string) *int8
+	// ReadNullableInt16 reads and returns an int16 pointer.
+	// The returned pointer may be nil.
 	ReadNullableInt16(fieldName string) *int16
+	// ReadNullableInt32 reads and returns an int32 pointer.
+	// The returned pointer may be nil.
 	ReadNullableInt32(fieldName string) *int32
+	// ReadNullableInt64 reads and returns an int64 pointer.
+	// The returned pointer may be nil.
 	ReadNullableInt64(fieldName string) *int64
+	// ReadNullableFloat32 reads and returns a float32 pointer.
+	// The returned pointer may be nil.
 	ReadNullableFloat32(fieldName string) *float32
+	// ReadNullableFloat64 reads and returns a float64 pointer.
+	// The returned pointer may be nil.
 	ReadNullableFloat64(fieldName string) *float64
+	// ReadArrayOfNullableBoolean reads and returns a slice of bool pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfNullableBoolean(fieldName string) []*bool
+	// ReadArrayOfNullableInt8 reads and returns a slice of int8 pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfNullableInt8(fieldName string) []*int8
+	// ReadArrayOfNullableInt16 reads and returns a slice of int16 pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfNullableInt16(fieldName string) []*int16
+	// ReadArrayOfNullableInt32 reads and returns a slice of int32 pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfNullableInt32(fieldName string) []*int32
+	// ReadArrayOfNullableInt64 reads and returns a slice of int64 pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfNullableInt64(fieldName string) []*int64
+	// ReadArrayOfNullableFloat32 reads and returns a slice of float32 pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfNullableFloat32(fieldName string) []*float32
+	// ReadArrayOfNullableFloat64 reads and returns a slice of float64 pointers.
+	// The returned pointers may be nil.
 	ReadArrayOfNullableFloat64(fieldName string) []*float64
+	// GetFieldKind returns the kind of the given field.
+	// If the field does not exist, it returns FieldKindNotAvailable
 	GetFieldKind(fieldName string) FieldKind
 }
 
+// CompactWriter is an interface implemented by types passed to CompactSerializer.Write method.
 type CompactWriter interface {
+	// WriteBoolean writes a bool value.
 	WriteBoolean(fieldName string, value bool)
+	// WriteInt8 writes an int8 value.
 	WriteInt8(fieldName string, value int8)
+	// WriteInt16 writes an int16 value.
 	WriteInt16(fieldName string, value int16)
+	// WriteInt32 writes an int32 value.
 	WriteInt32(fieldName string, value int32)
+	// WriteInt64 writes an int64 value.
 	WriteInt64(fieldName string, value int64)
+	// WriteFloat32 writes a float32 value.
 	WriteFloat32(fieldName string, value float32)
+	// WriteFloat64 writes a float64 value.
 	WriteFloat64(fieldName string, value float64)
+	// WriteString writes a string pointer value.
+	// The pointer may be nil.
 	WriteString(fieldName string, value *string)
+	// WriteDecimal writes a types.Decimal pointer value.
+	// The pointer may be nil.
 	WriteDecimal(fieldName string, value *types.Decimal)
+	// WriteTime writes a types.LocalTime pointer value.
+	// The pointer may be nil.
 	WriteTime(fieldName string, value *types.LocalTime)
+	// WriteDate writes a types.LocalDate pointer value.
+	// The pointer may be nil.
 	WriteDate(fieldName string, value *types.LocalDate)
+	// WriteTimestamp writes a types.LocalDateTime pointer value.
+	// The pointer may be nil.
 	WriteTimestamp(fieldName string, value *types.LocalDateTime)
+	// WriteTimestampWithTimezone writes a types.OffsetDateTime pointer value.
+	// The pointer may be nil.
 	WriteTimestampWithTimezone(fieldName string, value *types.OffsetDateTime)
+	// WriteCompact writes a value which can be serialized by a CompactSerializer.
 	WriteCompact(fieldName string, value interface{})
+	// WriteArrayOfBoolean writes a slice of bools.
 	WriteArrayOfBoolean(fieldName string, value []bool)
+	// WriteArrayOfInt8 writes a slice of int8s.
 	WriteArrayOfInt8(fieldName string, value []int8)
+	// WriteArrayOfInt16 writes a slice of int16s.
 	WriteArrayOfInt16(fieldName string, value []int16)
+	// WriteArrayOfInt32 writes a slice of int32s.
 	WriteArrayOfInt32(fieldName string, value []int32)
+	// WriteArrayOfInt64 writes a slice of int64s.
 	WriteArrayOfInt64(fieldName string, value []int64)
+	// WriteArrayOfFloat32 writes a slice of float32s.
 	WriteArrayOfFloat32(fieldName string, value []float32)
+	// WriteArrayOfFloat64 writes a slice of float64s.
 	WriteArrayOfFloat64(fieldName string, value []float64)
+	// WriteArrayOfString writes a slice of string pointers.
+	// The pointers may be nil.
 	WriteArrayOfString(fieldName string, value []*string)
+	// WriteArrayOfDecimal writes a slice of types.Decimal pointers.
+	// The pointers may be nil.
 	WriteArrayOfDecimal(fieldName string, value []*types.Decimal)
+	// WriteArrayOfTime writes a slice of types.LocalTime pointers.
+	// The pointers may be nil.
 	WriteArrayOfTime(fieldName string, value []*types.LocalTime)
+	// WriteArrayOfDate writes a slice of types.LocalDate pointers.
+	// The pointers may be nil.
 	WriteArrayOfDate(fieldName string, value []*types.LocalDate)
+	// WriteArrayOfTimestamp writes a slice of types.LocalDateTime pointers.
+	// The pointers may be nil.
 	WriteArrayOfTimestamp(fieldName string, value []*types.LocalDateTime)
+	// WriteArrayOfTimestampWithTimezone writes a slice of types.OffsetDateTime pointers.
+	// The pointers may be nil.
 	WriteArrayOfTimestampWithTimezone(fieldName string, value []*types.OffsetDateTime)
+	// WriteArrayOfCompact writes a slice of values which can be serialized using a CompactSerializer.
 	WriteArrayOfCompact(fieldName string, value []interface{})
+	// WriteNullableBoolean writes a bool pointer.
+	// The pointer may be nil.
 	WriteNullableBoolean(fieldName string, value *bool)
+	// WriteNullableInt8 writes an int8 pointer.
+	// The pointer may be nil.
 	WriteNullableInt8(fieldName string, value *int8)
+	// WriteNullableInt16 writes an int16 pointer.
+	// The pointer may be nil.
 	WriteNullableInt16(fieldName string, value *int16)
+	// WriteNullableInt32 writes an int32 pointer.
+	// The pointer may be nil.
 	WriteNullableInt32(fieldName string, value *int32)
+	// WriteNullableInt64 writes an int64 pointer.
+	// The pointer may be nil.
 	WriteNullableInt64(fieldName string, value *int64)
+	// WriteNullableFloat32 writes a float32 pointer.
+	// The pointer may be nil.
 	WriteNullableFloat32(fieldName string, value *float32)
+	// WriteNullableFloat64 writes a float64 pointer.
+	// The pointer may be nil.
 	WriteNullableFloat64(fieldName string, value *float64)
+	// WriteArrayOfNullableBoolean writes a slice of bool pointers.
+	// The pointers may be nil.
 	WriteArrayOfNullableBoolean(fieldName string, value []*bool)
+	// WriteArrayOfNullableInt8 writes a slice of int8 pointers.
+	// The pointers may be nil.
 	WriteArrayOfNullableInt8(fieldName string, value []*int8)
+	// WriteArrayOfNullableInt16 writes a slice of int16 pointers.
+	// The pointers may be nil.
 	WriteArrayOfNullableInt16(fieldName string, value []*int16)
+	// WriteArrayOfNullableInt32 writes a slice of int32 pointers.
+	// The pointers may be nil.
 	WriteArrayOfNullableInt32(fieldName string, value []*int32)
+	// WriteArrayOfNullableInt64 writes a slice of int64 pointers.
+	// The pointers may be nil.
 	WriteArrayOfNullableInt64(fieldName string, value []*int64)
+	// WriteArrayOfNullableFloat32 writes a slice of float32 pointers.
+	// The pointers may be nil.
 	WriteArrayOfNullableFloat32(fieldName string, value []*float32)
+	// WriteArrayOfNullableFloat64 writes a slice of float64 pointers.
+	// The pointers may be nil.
 	WriteArrayOfNullableFloat64(fieldName string, value []*float64)
 }
