01511479b3436455db1d9ec2cde468ed505b9183
Jack <jack.green@hazelcast.com>
2024-08-07 09:48:36 +0100

    Fix Java installation [1.4.z]
    The [tests are failing](https://github.com/hazelcast/hazelcast-go-client/actions/runs/10175906206/job/28144201884) because of a conflict between:
    - `JAVA_HOME`
    - Multiple Java installations on the runner
    
    Based on my [investigation and testing](https://github.com/hazelcast/client-compatibility-suites/pull/127#issuecomment-2272140720) using `setup-java`, and avoiding hardcoding, resolves these issues.

--- rc.sh
+++ rc.sh
@@ -183,15 +183,6 @@ echo "Java version:"
 java -version
 which java
 echo "JAVA_HOME: $JAVA_HOME"
-echo
-
-echo "Use Java 17"
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-java -version
-which java
-echo "JAVA_HOME: $JAVA_HOME"
-echo
 
 
 case "${1:-}" in
