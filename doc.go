c4f28f2c4df11e712dd3632cfc9e9028e8695400
Yüce Tekol <yuce.tekol@hazelcast.com>
2025-11-11 09:27:21 +0300

    Updated Docs (#1037)

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2025, Hazelcast, Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
@@ -17,29 +17,30 @@
 /*
 Package hazelcast provides the Hazelcast Go client.
 
-Hazelcast is an open-source distributed in-memory data store and computation platform. It provides a wide variety of distributed data structures and concurrency primitives.
+Hazelcast is an open-source distributed in-memory data store and computation platform.
+It provides a wide variety of distributed data structures and concurrency primitives.
 
-Hazelcast Go client is a way to communicate to Hazelcast IMDG clusters and access the cluster data.
+Hazelcast Go client is a way to communicate to Hazelcast clusters and access the cluster data.
 
 # Configuration
 
-If you are using Hazelcast and Go Client on the same computer, generally the default configuration should be fine.
+If you are using Hazelcast and the Go Client on the same computer, generally the default configuration should be fine.
 This is great for trying out the client.
 However, if you run the client on a different computer than any of the cluster members, you may need to do some simple configurations such as specifying the member addresses.
 
 The Hazelcast members and clients have their own configuration options.
 You may need to reflect some of the member side configurations on the client side to properly connect to the cluster.
 
-In order to configure the client, you only need to create a new `hazelcast.Config{}`, which you can pass to `hazelcast.StartNewClientWithConnfig` function:
+In order to configure the client, you only need to create a new [hazelcast.Config], which you can pass to [hazelcast.StartNewClientWithConfig] function:
 
-	config := hazelcast.Config{}
+	var config hazelcast.Config
 	client, err := hazelcast.StartNewClientWithConfig(context.TODO(), config)
 
-Calling hazelcast.StartNewClientWithConfig with the default configuration is equivalent to hazelcast.StartNewClient.
-The default configuration assumes Hazelcast is running at localhost:5701 with the cluster name set to dev.
+Calling [hazelcast.StartNewClientWithConfig] with the default configuration is equivalent to calling [hazelcast.StartNewClient].
+The default configuration assumes Hazelcast is running at localhost:5701 with the cluster name set to "dev".
 If you run Hazelcast members in a different server than the client, you need to make certain changes to client settings.
 
-Assuming Hazelcast members are running at hz1.server.com:5701, hz2.server.com:5701 and hz3.server.com:5701 with cluster name production, you would use the configuration below.
+Assuming Hazelcast members are running at hz1.server.com:5701, hz2.server.com:5701 and hz3.server.com:5701 with cluster name "production", you would use the configuration below.
 Note that addresses must include port numbers:
 
 	config := hazelcast.Config{}
@@ -86,7 +87,7 @@ You can simplify the code above by getting a reference to config.Cluster and upd
 Note that you should get a reference to the configuration section you are updating, otherwise you would update a copy of it, which doesn't modify the configuration.
 
 There are a few options that require a duration, such as config.Cluster.HeartbeatInterval, config.Cluster.Network.ConnectionTimeout and others.
-You must use types.Duration instead of time.Duration with those options, since types.Duration values support human readable durations when deserialized from text:
+You must use [types.Duration] instead of [time.Duration] with those options, since [types.Duration] values support human readable durations when deserialized from text:
 
 	import "github.com/hazelcast/hazelcast-go-client/types"
 	// ...
@@ -94,7 +95,7 @@ You must use types.Duration instead of time.Duration with those options, since t
 	config.Cluster.InvocationTimeout = types.Duration(3 * time.Minute)
 	config.Cluster.Network.ConnectionTimeout = types.Duration(10 * time.Second)
 
-That corresponds to the following JSON configuration. Refer to https://golang.org/pkg/time/#ParseDuration for the available duration strings:
+That corresponds to the following JSON configuration. Refer to [time.ParseDuration] for the available duration strings:
 
 	{
 		"Cluster": {
@@ -107,9 +108,12 @@ That corresponds to the following JSON configuration. Refer to https://golang.or
 Here are all configuration items with their default values:
 
 	config := hazelcast.Config{}
-	config.ClientName = ""
-	config.SetLabels()
 
+	// client configuration
+	config.ClientName = ""
+	config.SetLabels()  // empty slice of strings
+
+	// connection configuration
 	cc := &config.Cluster
 	cc.Name = "dev"
 	cc.HeartbeatTimeout = types.Duration(5 * time.Second)
@@ -123,10 +127,12 @@ Here are all configuration items with their default values:
 	cc.Network.SSL.Enabled = true
 	cc.Network.SSL.SetTLSConfig(&tls.Config{})
 	cc.Network.ConnectionTimeout = types.Duration(5 * time.Second)
+	cc.Network.SetPortRange(5701, 5703)
 
 	cc.Security.Credentials.Username = ""
 	cc.Security.Credentials.Password = ""
 
+	cc.Discovery.Strategy = nil
 	cc.Discovery.UsePublicIP = false
 
 	cc.Cloud.Enabled = false
@@ -139,6 +145,12 @@ Here are all configuration items with their default values:
 	cc.ConnectionStrategy.Retry.Multiplier = 1.05
 	cc.ConnectionStrategy.Retry.Jitter = 0.0
 
+	// failover configuration
+	config.Failover.Enabled = false
+	config.Failover.TryCount = math.MaxInt
+	config.Failover.SetConfigs() // empty slice of Config
+
+	// serialization configuration
 	config.Serialization.PortableVersion = 0
 	config.Serialization.LittleEndian = false
 	config.Serialization.SetPortableFactories()
@@ -147,16 +159,19 @@ Here are all configuration items with their default values:
 	config.Serialization.SetClassDefinitions()
 	config.Serialization.SetGlobalSerializer() // Gob serializer
 
+	// statistics configuration
 	config.Stats.Enabled = false
 	config.Stats.Period = types.Duration(5 * time.Second)
 
+	// logger configuration
+	config.Logger.CustomLogger = nil
 	config.Logger.Level = logger.InfoLevel
 
 Checkout the nearcache package for the documentation about the Near Cache.
 
 # Listening for Distributed Object Events
 
-You can listen to creation and destroy events for distributed objects by attaching a listener to the client.
+You can listen to creation and destroy events of distributed objects by attaching a listener to the client.
 A distributed object is created when first referenced unless it already exists.
 Here is an example:
 
@@ -178,27 +193,26 @@ If you don't want to receive any distributed object events, use client.RemoveDis
 
 # Running SQL Queries
 
-Running SQL queries require Hazelcast 5.0 and up.
+The client supports two ways of performing SQL queries:
+ 1. Using the [Client.SQL] service
+ 2. Using the [database/sql] compatible [github.com/hazelcast/hazelcast-go-client/sql/driver] driver
+
+This section explains how to use the [Client.SQL] service.
+
+Running SQL queries requires Hazelcast 5.0 and up.
 Check out the Hazelcast SQL documentation here: https://docs.hazelcast.com/hazelcast/latest/sql/sql-overview
 
-The SQL support should be enabled in Hazelcast server configuration:
+Hazelcast Jet support must be enabled in Hazelcast server configuration in order to use the SQL functionality:
 
 	<hazelcast>
 		<jet enabled="true" />
 	</hazelcast>
 
-The client supports two kinds of queries: The ones returning rows (select statements and a few others) and the rest (insert, update, etc.).
-The former kinds of queries are executed with QuerySQL method and the latter ones are executed with ExecSQL method.
-
-Use the question mark (?) for placeholders.
-
-To connect to a data source and query it as if it is a table, a mapping should be created.
-Currently, mappings for Map, Kafka and file data sources are supported.
-
-You can read the details about mappings here: https://docs.hazelcast.com/hazelcast/latest/sql/sql-overview#mappings
+To connect to a data source and query it as if it is a table, a mapping must be created.
+You can read the details about mappings in the [Mappings] documentation.
 
 The following data types are supported when inserting/updating.
-The names in parantheses correspond to SQL types:
+The names in parentheses correspond to SQL types:
 
   - string (varchar)
   - int8 (tinyint)
@@ -216,8 +230,6 @@ The names in parantheses correspond to SQL types:
   - types.OffsetDateTime (timestamp with time zone)
   - serialization.JSON (json)
 
-Using Date/Time
-
 In order to force using a specific date/time type, create a time.Time value and cast it to the target type:
 
 	t := time.Now()
@@ -225,6 +237,16 @@ In order to force using a specific date/time type, create a time.Time value and 
 	timeValue := types.LocalTime(t)
 	dateTimeValue := types.LocalDateTime(t)
 	dateTimeWithTimezoneValue := types.OffsetDateTime(t)
+
+To run SQL queries, use the client.SQL().Execute method:
+
+	query := "SELECT * FROM person"
+	res, err := client.SQL().Execute(ctx, query)
+
+If you need to inject a value to the query, use one question mark (?) for each value, and pass the actual values as arguments to [sql.Service.Execute]:
+
+	query := "SELECT name FROM person WHERE id=?"
+	res, err := client.SQL().ExecuteStatement(ctx, query, "sample-id")
 
 # Management Center Integration
 
@@ -234,10 +256,12 @@ You can enable statistics by setting config.Stats.Enabled to true.
 Optionally, the period of statistics collection can be set using config.Stats.Period setting.
 The labels set in configuration appear in the Management Center console:
 
-	config := hazelcast.Config{}
+	var config hazelcast.Config
 	config.SetLabels("fast-cache", "staging")
 	config.Stats.Enabled = true
 	config.Stats.Period = 1 * time.Second
 	client, err := hazelcast.StartNewClientWithConfig(config)
+
+[Mappings]: https://docs.hazelcast.com/hazelcast/latest/sql/sql-overview#mappings
 */
 package hazelcast
